8e7f9654e62760005ef7e74a92d11ea0b057cb5d
GauthamS21 <66888816+GauthamS21@users.noreply.github.com>
2021-06-17 01:26:11 +0530

    updated

---
+++ src/pages/api/webhook.js
@@ -0,0 +1,70 @@
+import { buffer } from "micro";
+import * as admin from "firebase-admin";
+
+const serviceAccount = require("../../../permissions.json");
+
+// sequre a conn to firebase from backend
+const app = !admin.apps.length
+  ? admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount),
+    })
+  : admin.app();
+
+// establish conn to stripe
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+const endpointSecret = process.env.STRIPE_SIGNING_SECRET;
+
+const fulfillOrder = async (session) => {
+  //console.log('Fulfilling order', session)
+
+  return app
+    .firestore()
+    .collection("users")
+    .doc(session.metadata.email)
+    .collection("orders")
+    .doc(session.id)
+    .set({
+      amount: session.amount_total / 100,
+      amount_shipping: session.total_details.amount_shipping / 100,
+      images: JSON.parse(session.metadata.images),
+      timestamp: admin.firestore.FieldValue.serverTimestamp(),
+    })
+    .then(() => {
+      console.log(`SUCCESS: Order ${session.id} has been added  to the DB`);
+    });
+};
+
+export default async (req, res) => {
+  if (req.method === "POST") {
+    const requestBuffer = await buffer(req);
+    const payload = await requestBuffer.toString();
+    const sig = req.headers["stripe-signature"];
+
+    let event;
+
+    //verify that EVENT posted came from stripe
+    try {
+      event = stripe.webhook.construction(payload, sig, endpointSecret);
+    } catch (err) {
+      console.log("Error", err.message);
+      return res.status(400).send(`Webhook error: ${err.message}`);
+    }
+    // HAndel checkout Session.comntpleted eve
+    if (event.type === "checkout.session.completed") {
+      const session = event.data.object;
+
+      //Fulfill the order
+      return fulfillOrder(session)
+        .then(() => res.status(200))
+        .catch((err) => res.status(400).send(`Webhook Error: ${err.message}`));
+    }
+  }
+};
+
+export const config = {
+  api: {
+    bodyParser: false,
+    externalResolver: true,
+  },
+};
