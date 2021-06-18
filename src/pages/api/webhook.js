d2fd625f49406534467e730e5364c0489778ecbc
GauthamS21 <66888816+GauthamS21@users.noreply.github.com>
2021-06-18 20:11:38 +0530

    Update webhook.js

--- src/pages/api/webhook.js
+++ src/pages/api/webhook.js
@@ -1,26 +1,23 @@
 import { buffer } from "micro";
 import * as admin from "firebase-admin";
+import { session } from "next-auth/client";
+import { flushSync } from "react-dom";
 
+// Connection to Firebase
 const serviceAccount = require("../../../permissions.json");
 
-// sequre a conn to firebase from backend
 const app = !admin.apps.length
-  ? admin.initializeApp({
-      credential: admin.credential.cert(serviceAccount),
-    })
+  ? admin.initializeApp({ credential: admin.credential.cert(serviceAccount) })
   : admin.app();
 
-// establish conn to stripe
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
-
 const endpointSecret = process.env.STRIPE_SIGNING_SECRET;
 
-const fulfillOrder = async (session) => {
-  console.log("Fulfilling order", session);
-
+const fullfillOrder = async (session) => {
+  console.log("Working On Order", session);
   return app
     .firestore()
-    .collection("users")
+    .collection("user")
     .doc(session.metadata.email)
     .collection("orders")
     .doc(session.id)
@@ -31,33 +28,33 @@ const fulfillOrder = async (session) => {
       timestamp: admin.firestore.FieldValue.serverTimestamp(),
     })
     .then(() => {
-      console.log(`SUCCESS: Order ${session.id} has been added `);
+      console.log(`Success Order : ${session.id} has been added`);
     });
 };
-
 export default async (req, res) => {
   if (req.method === "POST") {
     const requestBuffer = await buffer(req);
-    const payload = await requestBuffer.toString();
+    const payload = requestBuffer.toString();
     const sig = req.headers["stripe-signature"];
 
     let event;
 
-    //verify that EVENT posted came from stripe
+    // Verify the event posted came from stripe
     try {
-      event = stripe.webhook.construction(payload, sig, endpointSecret);
+      event = stripe.webhooks.constructEvent(payload, sig, endpointSecret);
     } catch (err) {
-      console.log("Error", err.message);
-      return res.status(400).send(`Webhook error: ${err.message}`);
+      console.log("ERROR", err.message);
+      return res.status(400).send(`WeebHook ERROR : ${err.message}`);
     }
-    // HAndel checkout Session.comntpleted eve
+
+    // Handle the checkout.session.completed event
     if (event.type === "checkout.session.completed") {
       const session = event.data.object;
 
-      //Fulfill the order
-      return fulfillOrder(session)
+      // FulFill the order
+      return fullfillOrder(session)
         .then(() => res.status(200))
-        .catch((err) => res.status(400).send(`Webhook Error: ${err.message}`));
+        .catch((err) => res.status(400).send(`WebHook ERROR: ${err.message}`));
     }
   }
 };
